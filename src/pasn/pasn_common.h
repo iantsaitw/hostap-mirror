218008e4b1efb6d6975b8016f2b2c8f26b7719af
rtk <rtk.wfa26@gmail.com>
2026-03-04 13:09:11 -0800

    EPPKE: append initail PMKID if pmkid_rnd_initial is set
    Able to pass PE-16.5

--- src/pasn/pasn_common.h
+++ src/pasn/pasn_common.h
@@ -73,6 +73,9 @@ struct pasn_data {
 	u8 custom_pmkid[PMKID_LEN];
 	enum mfp_options ieee80211w;
 
+	bool pmkid_rnd_initial_en;
+	u8 pmkid_rnd[PMKID_LEN];
+
 	/* Counter from the decrypted password identifier blob (set when
 	 * get_pt_for_pw_id() resolves an encrypted identifier) */
 	unsigned int sae_pw_id_counter;
@@ -334,6 +337,7 @@ void pasn_set_rsne(struct pasn_data *pasn, const u8 *rsne);
 void pasn_set_rsnxe_caps(struct pasn_data *pasn, u64 rsnxe_capab);
 void pasn_set_rsnxe_ie(struct pasn_data *pasn, const u8 *rsnxe_ie);
 void pasn_set_custom_pmkid(struct pasn_data *pasn, const u8 *pmkid);
+void pasn_set_rnd_pmkid(struct pasn_data *pasn, const u8 *pmkid);
 int pasn_set_extra_ies(struct pasn_data *pasn, const u8 *extra_ies,
 		       size_t extra_ies_len);
 void pasn_set_mfp(struct pasn_data *pasn, enum mfp_options mfp);
